d0fa5d6dc21941d915774b16b0414559f426d3fb
unknown <arahmand99@gmail.com>
2026-07-20 16:02:12 +0700

    Add Command & Conquer Yuri's Revenge to Arcade Classics

--- emulator.js
+++ emulator.js
@@ -132,6 +132,7 @@ function loadShareware(gameId, embedUrl) {
     if (gameId === 'doom') gameTitle = 'DOOM (Shareware 1993)';
     else if (gameId === 'simcity') gameTitle = 'SimCity 2000 (Demo 1993)';
     else if (gameId === 'pop') gameTitle = 'Prince of Persia (MS-DOS 1990)';
+    else if (gameId === 'ra2') gameTitle = "Command & Conquer: Yuri's Revenge & Red Alert";
 
     title.innerText = gameTitle;
     iframe.src = embedUrl;
